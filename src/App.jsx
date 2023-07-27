314927142e42fe242fc408d2a6b1b5d13962e166
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-07-27 14:29:11 +0700

    lesson 336 using grid to define row and column

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
@@ -7,45 +7,63 @@ import Settings from './pages/Settings';
 import NewUsers from './pages/Users';
 import Cabins from './pages/Cabins';
 import Bookings from './pages/Bookings';
+import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="dashboard"
-          element={<Dashboard />}
-        />
-        <Route
-          path="bookings"
-          element={<Bookings />}
-        />
-        <Route
-          path="cabins"
-          element={<Cabins />}
-        />
-        <Route
-          path="users"
-          element={<NewUsers />}
-        />
-        <Route
-          path="settings"
-          element={<Settings />}
-        />
-        <Route
-          path="account"
-          element={<Account />}
-        />
-        <Route
-          path="login"
-          element={<Login />}
-        />
-        <Route
-          path="*"
-          element={<PageNotFound />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route
+              index
+              element={
+                <Navigate
+                  replace
+                  to="dashboard"
+                />
+              }
+            />
+            <Route
+              path="dashboard"
+              element={<Dashboard />}
+            />
+            <Route
+              path="bookings"
+              element={<Bookings />}
+            />
+            <Route
+              path="cabins"
+              element={<Cabins />}
+            />
+            <Route
+              path="users"
+              element={<NewUsers />}
+            />
+            <Route
+              path="settings"
+              element={<Settings />}
+            />
+            <Route
+              path="account"
+              element={<Account />}
+            />
+          </Route>
+
+          <Route
+            path="login"
+            element={<Login />}
+          />
+
+          <Route
+            path="*"
+            element={<PageNotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
