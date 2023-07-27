2781ea3b330520f52550c6a70509a03b53083620
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-07-27 16:12:06 +0700

    setting storage buckets

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,10 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://xepdpqripjdpupkocdui.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-07-27T09%3A09%3A10.942Z"
+        alt=""
+      />
     </Row>
   );
 }
