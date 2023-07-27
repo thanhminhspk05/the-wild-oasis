8dfc114b23ae8592032b6c4ef6f3bd3e16d98914
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-07-27 13:40:18 +0700

    lesson 333

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -14,22 +15,36 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Checkin and Checkout</Heading>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Checkin and Checkout</Heading>
+              <Button onClick={() => alert('Checkin')}>Checkin</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert('Checkout')}
+              >
+                Checkout
+              </Button>
+            </div>
+          </Row>
 
-        <Button onClick={() => alert('Checkin')}>Checkin</Button>
-        <Button onClick={() => alert('Checkout')}>Checkout</Button>
-
-        <Heading as="h3">Form</Heading>
-        <Input
-          type="number"
-          placeholder="Number of guests"
-        />
-
-        <Input
-          type="number"
-          placeholder="Number of guests"
-        />
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form action="">
+              <Input
+                type="number"
+                placeholder="Number of guests"
+              />
+              <Input
+                type="number"
+                placeholder="Number of guests"
+              />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
