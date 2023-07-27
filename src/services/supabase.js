88fb5eaead47ecfd7fd7a77fd595e8010a2b8db0
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-07-27 16:05:42 +0700

    call api from supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://xepdpqripjdpupkocdui.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlcGRwcXJpcGpkcHVwa29jZHVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA0NDQ3NjEsImV4cCI6MjAwNjAyMDc2MX0.hUtahA3k1_1JR2bh8jz5a92aP8EGn5HmUzXIp42U5m0';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
