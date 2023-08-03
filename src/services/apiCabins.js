dfb3f86a3fb17bafeeecb283a8a877277c04c7b9
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-08-03 18:20:40 +0700

    save

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -23,7 +23,33 @@ export async function createCabins(formData) {
     .select();
 
   if (error) {
-    console.error(error);
+    throw new Error('Cabins could not be created');
+  }
+
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage.from('cabin-images').upload(imageName, formData.image);
+
+  // 3.Delete the cabin IF there was an error uploading image
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    throw new Error('Cabin image could not be uploaded and the cabin was not created');
+  }
+
+  return data;
+}
+
+export async function editCabins(formData) {
+  const imageName = `${Math.random()}-${formData.image.name}`.replaceAll('/', '');
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Update cabin
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert({ ...formData, image: imagePath })
+    .select();
+
+  if (error) {
     throw new Error('Cabins could not be created');
   }
 
